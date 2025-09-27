7670f572d023496b1468e1a4417420a8d7c791c6
Matheus <141074019+M00NH0RS3@users.noreply.github.com>
2025-09-27 01:19:07 +0000

    test1

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("fjur-penal-cache-v1").then((cache) => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
